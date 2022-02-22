22804ebeb09b5a614d1b9f4d4b323c5c0abf36a4
Camilo Garcia <gcgarciab@unal.edu.co>
2022-02-22 14:10:16 -0500

    Jest configurations

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   globals: {},
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': 'vue3-jest',
     '^.+\\js$': 'babel-jest',
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
